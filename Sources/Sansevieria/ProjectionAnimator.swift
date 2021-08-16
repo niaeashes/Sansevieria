84545c2b1bbf81ae32ce144da625c797ae1efabb
niaeashes <shota-nagasaki@cookpad.com>
2021-08-16 11:02:59 +0900

    Update start method

--- Sources/Sansevieria/ProjectionAnimator.swift
+++ Sources/Sansevieria/ProjectionAnimator.swift
@@ -23,15 +23,17 @@ public final class ProjectionAnimator {
         firstFrameTimestamp = CACurrentMediaTime()
     }
 
-    #if TARGET_OS_IPHONE
-
-    private weak var displayLink: CADisplayLink? = nil
-
     public func start() {
+        #if TARGET_OS_IPHONE
         let displayLink = CADisplayLink(target: self, selector: #selector(handleFrame(_:)))
         displayLink.add(to: .main, forMode: RunLoop.Mode.common)
         self.displayLink = displayLink
+        #endif
     }
+
+    #if TARGET_OS_IPHONE
+
+    private weak var displayLink: CADisplayLink? = nil
 
     @objc private func handleFrame(_ displayLink: CADisplayLink) {
         guard running else { return }
@@ -43,14 +45,6 @@ public final class ProjectionAnimator {
             running = false
             displayLink.invalidate()
         }
-    }
-
-    #endif
-
-    #if TARGET_OS_OSX
-
-    public func start() {
-        // TODO
     }
 
     #endif
